d35bcc870f8199e5c8361f3110fa8d9b7f8f7469
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-13 12:57:34 +0700

    Adding shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { ProductCart } from "@/components/product-cart";
 import { mockProducts } from "@/lib/mocks";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function HomePage() {
+    const products = await prisma.product.findMany({
+        include: { category: true },
+    });
+
     return (
         <main className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-6">Home</h1>
+            <h1 className="mb-6 text-3xl font-bold">Home</h1>
             <p>Showing {mockProducts.length} products</p>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                {mockProducts.map((product) => (
+                {products.map((product) => (
                     <ProductCart key={product.id} product={product} />
                 ))}
             </div>
--- components/product-cart.tsx
+++ components/product-cart.tsx
@@ -1,6 +1,6 @@
 import { formatPrice } from "@/lib/utils";
-import { Product } from "@/lib/mocks";
 import Image from "next/image";
+import { ProductModel } from "@/generated/prisma/models";
 
 /**
  * Image components was mean to be reduce bandwith
@@ -8,22 +8,23 @@ import Image from "next/image";
  * fill: The Next.js Image component extends the HTML <img> element for automatic image optimization.
  *
  */
-export function ProductCart({ product }: { product: Product }) {
+export function ProductCart({ product }: { product: ProductModel }) {
     return (
-        <div className="border border-green-200 rounded-lg p-4">
+        <div className="rounded-lg border border-green-200 p-4">
             <div className="relative aspect-video">
-                <Image
-                    quality={100}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    src={product.image}
-                    alt={product.name}
-                    className="object-cover"
-                />
+                {product.image && (
+                    <Image
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        src={product.image}
+                        alt={product.name}
+                        className="object-cover"
+                    />
+                )}
             </div>
             <h2 className="text-lg font-semibold">{product.name}</h2>
             <p className="text-gray-600">
-                {formatPrice(product.price)}
+                {formatPrice(Number(product.price))}
             </p>
             <p className="text-gray-500">{product.description}</p>
         </div>
